bb1892d083bdc3de2832c0262aecfcef3d917ef0
Rahul Vikram <vikramr@oregonstate.edu>
2024-12-04 19:32:17 -0800

    user authentication routing
    - handled client/server communication
    - handled vue routing via express user auth

--- src/components/login.vue
+++ src/components/login.vue
@@ -7,17 +7,49 @@
     const router = useRouter()
     // focus on the email input
     const emailInput = ref(null)
+    const errorMessage = ref('') // focus on error message
     onMounted(() => {
         // This code runs after the component is mounted
         emailInput.value?.focus() // Auto-focus the email input
     })
 
-    const handleLogin = () => {
-        // TODO: make call to express endpoint for login validation
-        
-        // Navigate to dashboard after successful login
-        router.push('/dashboard')
+    // call to express endpoint for signup validation
+    async function signUpEndpoint(email, password) {
+        try {
+            // call fetch to send post request to express endpoint
+            const response = await fetch('http://localhost:3000/login', {
+                method: 'POST',
+                body: JSON.stringify({ email, password })
+            })
+
+            // process json response
+            const data = await response.json()
+            return data;
+        } catch (error) {
+            console.error('Error signing up:', error)
+        }
     }
+
+    const handleLogin = async () => {
+        // get the email and password from the input fields
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+        
+        // done in backend
+        // if email exists but password is incorrect, endpoint returns false
+        // if email does not exist, endpoint returns true (new user creation)
+        // if email exists and password is correct, endpoint returns true
+        // call the signup endpoint
+        const data = await signUpEndpoint(email, password);
+
+        // if data is true, navigate to dashboard
+        if (data) {
+            router.push('/dashboard')
+        } else {
+            errorMessage.value = 'Invalid email or password';
+        }
+    }
+
 </script>
 
 <template>
@@ -29,11 +61,17 @@
 
         <label for="password">Password</label>
         <input id="password" type="password" placeholder="Must be 8 characters">
-
+        
+        <!-- dynamically render error message if user inputs invalid email or password -->
+        <p v-if="errorMessage" class="error-message">{{ errorMessage }}</p>
+      
         <button @click="handleLogin">Login</button>
     </div>
 </template>
 
 <style scoped>
-
+.error-message {
+    color: red;
+    margin-top: 10px;
+}
 </style>
