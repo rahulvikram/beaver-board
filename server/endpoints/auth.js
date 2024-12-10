16471331cd5e3260f9ea77aba161f7b3449dfb5e
Leo Ueno <leo@roboflow.com>
2024-12-10 13:45:47 -0800

    Revert insecure HTTPS

--- server/endpoints/auth.js
+++ server/endpoints/auth.js
@@ -41,8 +41,8 @@ module.exports = async (app) => {
           { key: serviceAccount.private_key, passphrase: '' }, 
           { algorithm: 'RS256' }
         );
-        res.cookie('token', token, { maxAge: 24 * 60 * 60 * 1000 });
-      
+        res.cookie('token', token, { httpOnly: true, secure: true, maxAge: 24 * 60 * 60 * 1000 });
+              
         return res.status(200).json({ success: true, id: user.id });
       });
       
